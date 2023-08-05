5633b15cf3ec8b0d4014d95734019652fc5efb6f
Aspen Wilson <aspen.schell@gmail.com>
2023-08-05 15:57:20 -0700

    Completed lab

--- server/models.py
+++ server/models.py
@@ -1,5 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+
+# metadata = MetaData(naming_convention={
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
 
 db = SQLAlchemy()
 
@@ -7,3 +12,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Integer)
